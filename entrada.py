8a5f5e74d280e44482ad85900719bcaab740059d
Gilberto Volpe <gilvolpe@github.com>
2026-02-12 11:06:25 -0300

    atualizacao de entradas

---
+++ entrada.py
@@ -0,0 +1,17 @@
+'''
+Criar 3 variaveis e armazenar as informacoes
+de user, password e dominio
+Depois apresentar as 3 variaveis
+'''
+nome = input('Qua o seu nome? ')
+senha = input('Qua a sua senha? ')
+dominio = input('Qua o seu dominio? ')
+print('Ola ',nome, ', seja bem vindo ao meu mundo')
+print('Sua senha é: ', senha)
+print('O seu domínio é: ', dominio)
+
+
+email = nome + '@' + dominio
+print('Seu e-mai é :', email)
+
+
