c7f9ae4fdc897ca327bc860c0da5e60a81b44c50
Gilberto Volpe <gilvolpe@github.com>
2026-02-19 10:55:43 -0300

    final aula de string

--- entrada.py
+++ entrada.py
@@ -22,3 +22,22 @@ PALAVRA = 'JACA'
 #colocar a string como toda minuscula
 print('Colocando o texto em minuscula: ', PALAVRA.lower()) 
 
+#contar caracter da string
+palavra_contar = 'banana'
+print('Contar a letra b', palavra_contar.count('b') )
+print('Contar a letra a', palavra_contar.count('a') )
+print('Contar a letra n', palavra_contar.count('n') )
+
+x = palavra_contar.count('x')
+print('A letra x foi contada ', x)
+
+
+print(email)
+a_c = email.count('a')
+e_c = email.count('e')
+i_c = email.count('i')
+o_c = email.count('o')
+u_c = email.count('u')
+nova_senha = 'a' + str(a_c) + 'e'+ str(e_c) + 'i' + str(i_c) + 'o' + str(o_c) + 'u' + str(u_c)
+print(nova_senha)
+
